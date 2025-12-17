d8d3d9a8f393c2bdbf6446cf94d890095ac239a8
poleagui06 <jeanpoleagui06@gmail.com>
2025-12-17 12:38:14 -0500

    solución Ejercicio

---
+++ Ejemplosc3/src/paquete05/Ejercicio.java
@@ -0,0 +1,22 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package paquete05;
+
+/**
+ *
+ * @author Pole
+ */
+public class Ejercicio {
+    public static void main(String[] args){
+        String[] nombresEncargados = new String[5];
+        double[][] produccionMensual = new double[5][12];
+        double[] totalProduccion = new double[5];
+        
+        double maxProduccion = -1;
+        int indiceMasProductivo = 0;
+    
+    }
+    
+}
